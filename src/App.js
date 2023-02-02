d78efcaf2f8d56114a7af2cba92e92429f39cbb9
Zawar Mughal <zawarmughal420@gmail.com>
2023-02-02 19:12:37 +0500

    Complete class base todo app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./App.css";
-import Create from "./components/Create";
-import Create2 from "./components/Create2";
 import Copy from "./components/Copy";
 function App() {
   return (
     <div>
-      {/* <Create /> */}
-      {/* <Create2/> */}
       <Copy/>
     </div>
   );
